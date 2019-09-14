62dbc6e898de6c2b8faf6b4780168077b80a7be7
abc <abc@example.com>
2019-09-13 22:44:53 -0600

    diseño del loguin y el boton de facebook funcionando

--- app/src/main/java/com/example/mark5/login.java
+++ app/src/main/java/com/example/mark5/login.java
@@ -2,13 +2,62 @@ package com.example.mark5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class login extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class login extends AppCompatActivity
+{
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+        {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                goMenu();
+            }
+
+            @Override
+            public void onCancel()
+            {
+                Toast.makeText(getApplicationContext(), "Se Cancelo el registro", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+                Toast.makeText(getApplicationContext(), "Al parecer hubo un error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMenu()
+    {
+        Intent intent = new Intent(this, menu.class);
+        startActivity(intent);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
