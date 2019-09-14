225bd0c647f2b9d3aa94e6fbf4cba5e64115c37f
abc <abc@example.com>
2019-09-13 21:08:41 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mark5/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mark5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+            }
+        },1000);
+    }
+}
