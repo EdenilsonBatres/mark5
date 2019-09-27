d37118f4fa53561c0982481d994ecb0899c24384
abc <abc@example.com>
2019-09-26 20:04:05 -0600

    entrelazamiento de pantallas de pantallas

--- app/src/main/java/com/example/mark5/menusito.java
+++ app/src/main/java/com/example/mark5/menusito.java
@@ -1,10 +1,7 @@
 package com.example.mark5;
 
-import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import android.view.View;
 
@@ -13,6 +10,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import android.view.MenuItem;
 
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -20,7 +19,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.Menu;
+import android.widget.Toast;
 
 public class menusito extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -41,6 +40,18 @@ public class menusito extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        /*if (AccessToken.getCurrentAccessToken() == null)
+        {
+            goLogInScreen();
+        }*/
+    }
+
+    private void goLogInScreen()
+    {
+        Intent a_loguin = new Intent(this, login.class);
+        startActivity(a_loguin);
+        finish();
     }
 
     @Override
@@ -55,26 +66,80 @@ public class menusito extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        if (id == R.id.nav_tutorial)
+        {
+            Toast.makeText(this,"al tutorial", Toast.LENGTH_SHORT).show();
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        }
+        else if (id == R.id.nav_configuracion)
+        {
 
-        } else if (id == R.id.nav_slideshow) {
+        }
+        else if (id == R.id.nav_consejos)
+        {
 
-        } else if (id == R.id.nav_tools) {
+        }
+        else if (id == R.id.nav_sugerencias)
+        {
 
-        } else if (id == R.id.nav_share) {
+        }
+        else if (id == R.id.nav_ajustes)
+        {
 
-        } else if (id == R.id.nav_send) {
-
+        }
+        else if (id == R.id.nav_salir)
+        {
+            //LoginManager.getInstance().logOut();
+           System.exit(0);
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void onclick (View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.gesto_voz_btn:
+                Intent gesto_voz_btn = new Intent(this, gesto_voz.class);
+                startActivity(gesto_voz_btn);
+                break;
+            case R.id.gesto_texto_btn:
+                Intent gesto_texto_btn = new Intent(this, gesto_texto.class);
+                startActivity(gesto_texto_btn);
+                break;
+            case R.id.texto_voz_btn:
+                Intent texto_voz_btn = new Intent(this, texto_voz.class);
+                startActivity(texto_voz_btn);
+                break;
+            case R.id.texto_gestos_btn:
+                Intent texto_gestos_btn = new Intent(this, texto_gesto.class);
+                startActivity(texto_gestos_btn);
+                break;
+            case R.id.vos_gesto_btn:
+                Intent voz_gesto_btn = new Intent(this, voz_gesto.class);
+                startActivity(voz_gesto_btn);
+                break;
+            case R.id.voz_texto_btn:
+                Intent voz_texto_btn = new Intent(this, voz_texto.class);
+                startActivity(voz_texto_btn);
+                break;
+            case R.id.configuracion_btn:
+                Intent configiracion_btn= new Intent(this, configuracion.class);
+                startActivity(configiracion_btn);
+                break;
+            case R.id.consejos_btn:
+                Intent consejos_btn= new Intent(this, consejos.class);
+                startActivity(consejos_btn);
+                break;
+
+        }
     }
 }
